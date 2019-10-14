1f7012674ae532041505c41cc114174d863da4fc
harrisonwong <harrisonjwong@yahoo.com>
2019-10-13 23:22:21 -0400

    Change to hash router to fix statically loaded github pages problem

--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom'
-import Face from './Face';
-import Projects from './Projects';
-import Skills from './Skills';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Face from "./Face";
+import Projects from "./Projects";
+import Skills from "./Skills";
 
 export default class Main extends React.Component {
   render() {
     return (
       <main>
         <Switch>
-          <Route exact path='/' component={Face}/>
-          <Route path='/projects' component={Projects}/>
-          <Route path='/skills' component={Skills}/>
+          <Route exact path="/" component={Face} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/skills" component={Skills} />
         </Switch>
       </main>
     );
