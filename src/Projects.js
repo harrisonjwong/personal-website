2c159a5c203bb553e4c0168fbe934ae0c8419c16
harrisonwong <harrisonjwong@yahoo.com>
2019-10-27 13:53:14 -0400

    Change projects page to material ui grid list

--- src/Projects.js
+++ src/Projects.js
@@ -1,72 +1,78 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-import AnimatorPic from "./Animator.png";
-import MazeGamePic from "./MazeGame.png";
-import GreenLinePic1 from "./GreenLine1.png";
-import GreenLinePic2 from "./GreenLine2.png";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import IconButton from "@material-ui/core/IconButton";
+import InfoIcon from "@material-ui/icons/Info";
+
+import AnimatorPic from "./pictures/Animator2.png";
+import MazeGamePic from "./pictures/MazeGame.png";
+import GreenLinePic from "./pictures/GreenLine.png";
+import GreenLinePic1 from "./pictures/GreenLine1.png";
+import GreenLinePic2 from "./pictures/GreenLine2.png";
+import WebsitePic from "./pictures/Website.png";
+import TicTacToePic from "./pictures/TicTacToe.png";
 import "./Projects.css";
 
 export default class Projects extends React.Component {
   render() {
+    const tileData = [
+      {
+        img: AnimatorPic,
+        title: "Easy Animator",
+        language: "Java (Java Swing)"
+      },
+      {
+        img: GreenLinePic,
+        title: "Green Line Predictions iOS App",
+        language: "Swift"
+      },
+      {
+        img: WebsitePic,
+        title: "Personal Website",
+        language: "JavaScript (React)"
+      },
+      {
+        img: TicTacToePic,
+        title: "Tic-Tac-Toe",
+        language: "Swift"
+      },
+      {
+        img: MazeGamePic,
+        title: "Maze Generator and Solver",
+        language: "Java"
+      }
+    ];
     return (
-      <div className="projectsBackground">
-        <div className="wholeGrid">
-          <Grid container spacing={3}>
-            <Grid item xs={6}>
-              <Paper className="commonSquare animatorSquare">
-                <img
-                  src={AnimatorPic}
-                  className="animatorPic"
-                  alt="animator project"
-                />
-                <br />
-                Easy Animator
-              </Paper>
-            </Grid>
-            <Grid item xs={6}>
-              <Paper className="commonSquare greenLineSquare">
-                <img
-                  src={GreenLinePic1}
-                  className="greenLinePic"
-                  alt="green line app 1"
-                />
-                <img
-                  src={GreenLinePic2}
-                  className="greenLinePic"
-                  alt="green line app 2"
-                />
-                <br />
-                Green Line iOS App
-              </Paper>
-            </Grid>
-            <Grid item xs={6}>
-              <Paper className="commonSquare mazeGameSquare">
-                <img
-                  src={MazeGamePic}
-                  className="mazeGamePic"
-                  alt="maze game project"
-                />
-                <br />
-                Maze Game
-              </Paper>
-            </Grid>
-            <Grid item xs={6}>
-              <Paper className="commonSquare">xs=6</Paper>
-            </Grid>
-            <Grid item xs={6}>
-              <Paper className="commonSquare">xs=6</Paper>
-            </Grid>
-            <Grid item xs={6}>
-              <Paper className="commonSquare">xs=6</Paper>
-            </Grid>
-          </Grid>
-        </div>
+      <div className="imageGrid projectsBackground">
+        <GridList cellHeight={250} className="gridList">
+          {tileData.map(tile => (
+            <GridListTile key={tile.img}>
+              <img src={tile.img} alt={tile.title} />
+              <GridListTileBar
+                title={tile.title}
+                subtitle={<span>Language: {tile.language}</span>}
+                actionIcon={
+                  <IconButton
+                    aria-label={`info about ${tile.title}`}
+                    className="button"
+                  >
+                    <InfoIcon />
+                  </IconButton>
+                }
+              />
+            </GridListTile>
+          ))}
+        </GridList>
       </div>
     );
   }
 }
+
+// <GridListTile key="Subheader" cols={2} style={{ height: "auto" }}>
+//   <ListSubheader component="div">Projects</ListSubheader>
+// </GridListTile>
 /*
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
@@ -115,4 +121,60 @@ export default function CenteredGrid() {
     </div>
   );
 }
+*/
+
+/*
+<div className="projectsBackground">
+  <div className="wholeGrid">
+    <Grid container spacing={3}>
+      <Grid item xs={6}>
+        <Paper className="commonSquare animatorSquare">
+          <img
+            src={AnimatorPic}
+            className="animatorPic"
+            alt="animator project"
+          />
+          <br />
+          Easy Animator
+        </Paper>
+      </Grid>
+      <Grid item xs={6}>
+        <Paper className="commonSquare greenLineSquare">
+          <img
+            src={GreenLinePic1}
+            className="greenLinePic"
+            alt="green line app 1"
+          />
+          <img
+            src={GreenLinePic2}
+            className="greenLinePic"
+            alt="green line app 2"
+          />
+          <br />
+          Green Line iOS App
+        </Paper>
+      </Grid>
+      <Grid item xs={6}>
+        <Paper className="commonSquare mazeGameSquare">
+          <img
+            src={MazeGamePic}
+            className="mazeGamePic"
+            alt="maze game project"
+          />
+          <br />
+          Maze Game
+        </Paper>
+      </Grid>
+      <Grid item xs={6}>
+        <Paper className="commonSquare websiteSquare">xs=6</Paper>
+      </Grid>
+      <Grid item xs={6}>
+        <Paper className="commonSquare">xs=6</Paper>
+      </Grid>
+      <Grid item xs={6}>
+        <Paper className="commonSquare">xs=6</Paper>
+      </Grid>
+    </Grid>
+  </div>
+</div>
 */
