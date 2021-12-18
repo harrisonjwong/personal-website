71d29357a478ceeee4c69d0810b14f54f646370a
Harrison Wong <harrisonjwong@yahoo.com>
2021-12-17 20:43:42 -0500

    Update info

--- src/components/home.js
+++ src/components/home.js
@@ -5,14 +5,17 @@ const Home = () => {
   const GITHUB = 'https://github.com/harrisonjwong';
   const LINKEDIN = 'https://linkedin.com/in/harrisonjwong';
   const RESUME = 'https://raw.githubusercontent.com/harrisonjwong/resume/master/Harrison_Wong_Resume.pdf';
+  const CAPITAL_ONE = 'https://capitalone.com';
+  const CISCO = 'https://cisco.com';
+  const VERISK = 'https://poweradvocate.com';
   return (
     <div className='container-fluid'>
-      <h1>Welcome to Harrison Wong's website</h1>
+      <h1>Harrison Wong</h1>
       <h3>About</h3>
       <p>
-        I'm a software engineer and a graduate of <a href={NORTHEASTERN}>Northeastern University</a>.
+        I am currently a software engineer at <a href={CAPITAL_ONE}>Capital One</a>. Formerly I worked at <a href={CISCO}>Cisco</a> and <a href={VERISK}>Verisk (PowerAdvocate)</a>.
         <br/>
-        I completed my undergrad in May 2021 studying Computer Engineering and Computer Science.
+        I graduated from <a href={NORTHEASTERN}>Northeastern University</a> in May 2021 with a Bachelors in Computer Engineering and Computer Science.
       </p>
       <h3>Links</h3>
       <ul>
